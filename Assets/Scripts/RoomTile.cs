5e2b7dc0fa55006f87fec2d648776312c15ec053
mfaizudd <mfaizudd@gmail.com>
2019-08-25 15:43:56 +0700

    First commit

---
+++ Assets/Scripts/RoomTile.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTile : MonoBehaviour
+{
+    public CorridorTile[] corridorSpawns;
+    public Transform[] roomSpawns;
+    public GameObject roomPrefab;
+    public float roomCheckRadius = 1f;
+
+    public int RoomLeft
+    {
+        get
+        {
+            return corridorSpawns.Count(x => !x.HaveRoom);
+        }
+    }
+
+    public bool CheckLocationAvailable(Transform transform)
+    {
+        var room = Physics2D.OverlapCircle(transform.position, roomCheckRadius);
+        return room == null && room.CompareTag(roomPrefab.tag);
+    }
+}
